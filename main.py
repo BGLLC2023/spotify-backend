54da428dd6d300af887ca2f34b55ddb0669ccf81
Greg Lauray <greglauray@Gregs-MacBook-Pro-2.local>
2026-05-02 13:34:55 -0400

    cors update

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ app = FastAPI(lifespan=lifespan)
 # dictates front url that can access the beackend and do database operations 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PATCH", "DELETE"],
     allow_headers=["*"],
